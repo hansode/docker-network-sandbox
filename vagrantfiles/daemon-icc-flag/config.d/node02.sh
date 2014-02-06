839e8bdd7a741cd5e6561a1699f35629279683c6
Masahito Yoshida <masahito@axsh.net>
2014-02-06 17:00:28 +0900

    change synced folder mount point.

--- vagrantfiles/daemon-icc-flag/config.d/node02.sh
+++ vagrantfiles/daemon-icc-flag/config.d/node02.sh
@@ -17,14 +17,14 @@ ps -ef | grep [/]usr/bin/docker
 ## ct1
 
 sudo docker run -d -p 22 dhrp/sshd /usr/sbin/sshd -D
-sudo iptables -t filter -nL | tee /vagrant/$(hostname)_iptables.filter.1.log
-sudo iptables -t nat    -nL | tee /vagrant/$(hostname)_iptables.nat.1.log
+sudo iptables -t filter -nL | tee /vagrant/iptables.filter.1.log
+sudo iptables -t nat    -nL | tee /vagrant/iptables.nat.1.log
 
 ## ct2
 
 sudo docker run -d -p 22 dhrp/sshd /usr/sbin/sshd -D
-sudo iptables -t filter -nL | tee /vagrant/$(hostname)_iptables.filter.2.log
-sudo iptables -t nat    -nL | tee /vagrant/$(hostname)_iptables.nat.2.log
+sudo iptables -t filter -nL | tee /vagrant/iptables.filter.2.log
+sudo iptables -t nat    -nL | tee /vagrant/iptables.nat.2.log
 
 # teardown
 sudo docker kill $(sudo docker ps -q)
